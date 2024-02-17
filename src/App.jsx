53850e6621538c1a946812cd935d42729a09fd5d
Jorge Centeno Mesa <eljorgecenteno@gmail.com>
2024-02-17 12:01:46 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from './Components/NavBar'
 import {Route, Routes} from "react-router-dom"
 import HomePage from "./Pages/HomePage"
 import AllRecipesPage from "./Pages/AllRecipesPage"
+import RecipeDetailsPage from './Pages/RecipeDetailsPage'
+import CreateRecipePage from './Pages/CreateRecipePage'
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage></HomePage>}></Route>
         <Route path='/Allrecipes' element={<AllRecipesPage></AllRecipesPage>}></Route>
+        <Route path='/Allrecipes/:id' element={<RecipeDetailsPage></RecipeDetailsPage>}></Route>
+        <Route path='/CreateRecipe' element={<CreateRecipePage></CreateRecipePage>}></Route>
       </Routes>
     </>
   )
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,9 +1,14 @@
+import {Link} from "react-router-dom"
+
 function Navbar() {
 
 
     return (
       <>
-        <h1>Navbar aquí</h1>
+        <h1>Navbar Element</h1>
+        <Link to="/">Go to Homepage</Link>
+        <Link to="/Allrecipes">Search all recipes</Link>
+        <Link to="/CreateRecipe">Add a new recipe</Link>
       </>
     )
   }
