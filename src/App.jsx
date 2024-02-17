18c12c383ad0dbec9e3cfed13f8c2f0f7ecb113b
nenuphars <84506458+nenuphars@users.noreply.github.com>
2024-02-17 14:30:57 +0100

    Revert "Routing"

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './Components/NavBar'
-import {Route, Routes} from "react-router-dom"
-import HomePage from "./Pages/HomePage"
-import AllRecipesPage from "./Pages/AllRecipesPage"
-import RecipeDetailsPage from './Pages/RecipeDetailsPage'
-import CreateRecipePage from './Pages/CreateRecipePage'
 
 function App() {
-
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path='/' element={<HomePage></HomePage>}></Route>
-        <Route path='/Allrecipes' element={<AllRecipesPage></AllRecipesPage>}></Route>
-        <Route path='/Allrecipes/:id' element={<RecipeDetailsPage></RecipeDetailsPage>}></Route>
-        <Route path='/CreateRecipe' element={<CreateRecipePage></CreateRecipePage>}></Route>
-      </Routes>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
