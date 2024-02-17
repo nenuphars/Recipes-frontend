528fa30d684c1c05283e024ebc0d8af6a5391ad2
Jorge Centeno Mesa <eljorgecenteno@gmail.com>
2024-02-17 12:26:03 +0100

    basic routing done

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,13 @@
 function HomePage() {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <h2>
+        Here goes a description of this beautiful webpage blablalabblalblablal
+      </h2>
+      <h4>Here goes a link to a random recipe</h4>
+    </>
+  );
+}
 
-
-    return (
-      <>
-        <h1>Home Page</h1>
-      </>
-    )
-  }
-  
-  export default HomePage
-  
+export default HomePage;
