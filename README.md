bbe8e339b4e7b7f0a02cddf9b7320278e9155509
Nadine Rangosch <nrangosch@gmail.com>
2024-02-17 16:26:24 +0100

    added content and random recipe to homepage

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Recipes App
-You have some ingredients at home, but don't know what to cook? Searching for recipe's online can be overwhelming, when there are a million options to choose from. This app wants to simplify and speed up the process, so that you don't have to endlessly scroll until you become hangry before you even start cooking.
+
+## Description
+You have some ingredients at home, but don't know what to cook? Searching for recipe's online can be overwhelming, when there are a million options to choose from. This app wants to simplify and speed up the process, so that you don't have to endlessly scroll and become hangry before you even start cooking.
 
 ## MVP
 - [ ] create structure of files (pages, components, etc.)
-- [ ] set up mock backend with recipe data (name, ingredients, preparation steps, tags)
+- [ ] set up mock backend with recipe data (ID, photo URL, name, ingredients, quantity, preparation steps, tags)
 - [ ] Homepage component: explains the main functionality of the webpage
 - [ ] users can create a new recipe via a form
 - [ ] users can edit existing recipes
@@ -14,3 +16,17 @@ You have some ingredients at home, but don't know what to cook? Searching for re
 - [ ] users can search for a recipe by ingredients
 - [ ] users can search for a recipe by tags
 - [ ] users can comment on recipes
+
+
+## Technologies used
+
+HTML, CSS, Javascript, React, Vite
+
+
+
+## Collaborators
+
+[Jorge Centeno Mesa](https://github.com/eljorgecenteno)
+
+[Nadine Rangosch](https://github.com/nenuphars)
+
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,11 +1,51 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+
 function HomePage() {
+  const [allRecipes, setAllRecipes] = useState([])
+  const [randomRecipe, setRandomRecipe] = useState(null)
+
+
+
+
+  useEffect(()=>{
+    axios.get(`${import.meta.env.VITE_BASE_URL}/Recipes`)
+    .then((recipesFromAPI)=>{
+      console.log(recipesFromAPI.data)
+      setAllRecipes(recipesFromAPI.data)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }, [])
+
+  useEffect(()=>{
+    setRandomRecipe(allRecipes[Math.floor(Math.random() * allRecipes.length)])
+    console.log(randomRecipe)
+
+  }, [allRecipes, randomRecipe])
+  
+
+  
   return (
     <>
-      <h1>Home Page</h1>
-      <h2>
-        Here goes a description of this beautiful webpage blablalabblalblablal
-      </h2>
-      <h4>Here goes a link to a random recipe</h4>
+      <div>
+        <h3>Description: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, iusto dicta iste perspiciatis est molestias.</h3>
+        <h4>Feeling insecure?</h4>
+        <button onClick={()=>{setRandomRecipe(allRecipes[Math.floor(Math.random() * allRecipes.length)])}}>Get a random recipe</button>
+      </div>
+      <div>
+      {!randomRecipe && <p>...loading</p>}
+      {randomRecipe && (
+        <>
+          <h2>{randomRecipe.name}</h2>
+          <img src={randomRecipe.photo_URL} alt={randomRecipe.name} />
+          <p><strong>{randomRecipe.duration}</strong></p>
+          <p>{randomRecipe.description}</p>
+
+        </>
+      )}
+      </div>
     </>
   );
 }
