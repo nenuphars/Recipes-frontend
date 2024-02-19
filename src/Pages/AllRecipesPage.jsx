233ffdb02da71e6e132d12b94c2a78deff46dcdd
Jorge Centeno Mesa <eljorgecenteno@gmail.com>
2024-02-19 21:18:02 +0100

    some styling added

--- src/Pages/AllRecipesPage.jsx
+++ src/Pages/AllRecipesPage.jsx
@@ -1,36 +1,47 @@
-import { useEffect } from "react"
-import { Link } from "react-router-dom"
-import { useState } from "react"
-import axios from 'axios'
-import '../App.css'
- function AllRecipesPage() {
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import "../App.css";
+function AllRecipesPage() {
+  const [allRecipes, setAllRecipes] = useState([]);
 
-  const [allRecipes,setAllRecipes] = useState([])
-  
-  useEffect(()=>{
-    axios.get(`${import.meta.env.VITE_BASE_URL}/Recipes`)
-    .then((recipes)=>{setAllRecipes(recipes.data)
-    console.log(recipes.data)})
-    .catch((error)=>{
-     console.log(error)
-    })
-   },[])
-    return (
-      <>
-        <h1>All recipes Page</h1>
-        {allRecipes.map((eachRecipe)=>{
-          return(<h4 id="eachRecipe" className="eachRecipeItem" key={eachRecipe.id}>
-          {eachRecipe.name}
-          <img id="eachPhoto"src={eachRecipe.photo_URL} alt={`${eachRecipe.name} dish`} />
-          {eachRecipe.duration}
-          {eachRecipe.tags.map((eachTag)=>{return(<p key={eachTag}>{eachTag}</p>)})}
-          </h4>)
-        
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BASE_URL}/Recipes`)
+      .then((recipes) => {
+        setAllRecipes(recipes.data);
+        console.log(recipes.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return (
+    <>
+      <h1>All recipes Page</h1>
+      <div id="eachRecipeContainer">
+        {allRecipes.map((eachRecipe) => {
+          return (
+            <div id="eachRecipe" className="eachRecipeItem" key={eachRecipe.id}>
+              <img
+                id="eachPhoto"
+                src={eachRecipe.photo_URL}
+                alt={`${eachRecipe.name} dish`}
+              />
+              <h2>{eachRecipe.name}</h2>
 
+              <h4>⏱️ {eachRecipe.duration}</h4>
+              <div id="tagContainer">
+                {eachRecipe.tags.map((eachTag) => {
+                  return <div key={eachTag}>{eachTag}</div>;
+                })}
+              </div>
+            </div>
+          );
         })}
-      </>
-    )}
-  
-  
-  export default AllRecipesPage
-  
+      </div>
+    </>
+  );
+}
+
+export default AllRecipesPage;
