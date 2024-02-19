4b6f41528cde87f1e23c14d2e18596486b8eb2c4
Jorge Centeno Mesa <eljorgecenteno@gmail.com>
2024-02-19 18:27:09 +0100

    finished allrecipesPage

--- src/Pages/AllRecipesPage.jsx
+++ src/Pages/AllRecipesPage.jsx
@@ -1,13 +1,36 @@
-function AllRecipesPage() {
-  // Here we should map thourh each object of our data, showing to the user each as an individual element. 
-  
+import { useEffect } from "react"
+import { Link } from "react-router-dom"
+import { useState } from "react"
+import axios from 'axios'
+import '../App.css'
+ function AllRecipesPage() {
 
+  const [allRecipes,setAllRecipes] = useState([])
+  
+  useEffect(()=>{
+    axios.get(`${import.meta.env.VITE_BASE_URL}/Recipes`)
+    .then((recipes)=>{setAllRecipes(recipes.data)
+    console.log(recipes.data)})
+    .catch((error)=>{
+     console.log(error)
+    })
+   },[])
     return (
       <>
         <h1>All recipes Page</h1>
+        {allRecipes.map((eachRecipe)=>{
+          return(<h4 id="eachRecipe" className="eachRecipeItem" key={eachRecipe.id}>
+          {eachRecipe.name}
+          <img id="eachPhoto"src={eachRecipe.photo_URL} alt={`${eachRecipe.name} dish`} />
+          {eachRecipe.duration}
+          {eachRecipe.tags.map((eachTag)=>{return(<p key={eachTag}>{eachTag}</p>)})}
+          </h4>)
+        
+
+        })}
       </>
-    )
-  }
+    )}
+  
   
   export default AllRecipesPage
   
