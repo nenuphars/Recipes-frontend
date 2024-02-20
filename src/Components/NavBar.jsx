49a559671e0014661d22f22aff224fa45d7c4bd7
Nadine Rangosch <nrangosch@gmail.com>
2024-02-20 20:05:24 +0100

    styled navbar with MUI components

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,17 +1,49 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function Navbar() {
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+                  Home
+                </Link>
+              </Typography>
 
 
-    return (
-      <>
-        <h1>Navbar Element</h1>
-        <Link to="/">Go to Homepage</Link>
-        <Link to="/Allrecipes">Search all recipes</Link>
-        <Link to="/CreateRecipe">Add a new recipe</Link>
-      </>
-    )
-  }
-  
-  export default Navbar
-  
+      
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <Link
+                  to="/Allrecipes"
+                  style={{ color: "white", textDecoration: "none" }}
+                >
+                  All Recipes
+                </Link>
+              </Typography>
+
+
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+}
+
+export default Navbar;
