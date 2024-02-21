4bf29b6dc4bd9e3f38b8b6ba29b7ea016587f01a
Nadine Rangosch <nrangosch@gmail.com>
2024-02-21 19:43:31 +0100

    adjusted styles for random recipe, copied from allRecipesPage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,6 +1,13 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'
+import { Card } from '@mui/material'
 import './HomePage.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 function HomePage() {
   const [allRecipes, setAllRecipes] = useState([])
@@ -28,24 +35,33 @@ function HomePage() {
   return (
     <div id="homepage-container">
       <div className="homepage-wrapper">
-        <h3>When you look into your fridge, you have no inspiration?
-        What the Fridge wants to help you get out of this misery and prevent any hangriness due to not being able to decide what to cook.</h3>
+        <h3>When you look into your fridge, you have no inspiration? 
+        <span className="cursive"> What the Fridge </span>
+         wants to help you get out of this misery and prevent any hangriness due to not being able to decide what to cook.</h3>
         <h4>Feeling insecure?</h4>
         <button onClick={()=>{setRandomRecipe(allRecipes[Math.floor(Math.random() * allRecipes.length)])}}>Get a random recipe</button>
       </div>
       <div className="homepage-wrapper">
       {!randomRecipe && <p>...loading</p>}
       {randomRecipe && (
-        <>
-          <h2>{randomRecipe.name}</h2>
-          <div className='random-img-wrapper'>
-          <img src={randomRecipe.photo_URL} alt={randomRecipe.name} className='random-recipe-img'/>
+        <Link to={`/Allrecipes/${randomRecipe.id}`}  key={randomRecipe.id} style={{textDecoration:"none"}}>
+            <Card id="eachCard" >
+            
+              <img
+              className="random-img-wrapper"
+                src={randomRecipe.photo_URL}
+                alt={`${randomRecipe.name} dish`}
+              />
+              <h2>{randomRecipe.name}</h2>
 
-          </div>
-          <p><strong>{randomRecipe.duration}</strong></p>
-          <p>{randomRecipe.description}</p>
-
-        </>
+              <h4 >⏱️ {randomRecipe.duration}</h4>
+              <div id="tagContainer">
+                {randomRecipe.tags.map((eachTag) => {
+                  return <div key={eachTag}>{eachTag}</div>;
+                })}
+              </div>
+            </Card>
+            </Link>
       )}
       </div>
     </div>
