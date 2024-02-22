779c1ad07729f733b3e09a595562f22f180f9ca9
Nadine Rangosch <nrangosch@gmail.com>
2024-02-22 16:23:37 +0100

    styling without material ui

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,48 +1,30 @@
 import { Link } from "react-router-dom";
-import {
-  Box,
-  AppBar,
-  Toolbar,
-  Typography,
-  Button,
-  IconButton,
-} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
+import './NavBar.css'
+
 
 function Navbar() {
   return (
-    <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                <Link to="/" style={{ color: "white", textDecoration: "none" }}>
-                  Home
+    <header>
+      <div id="navbar-container">
+      <nav id="nav-links-wrapper">
+                <Link to="/" style={{ color: "mediumspringgreen", textDecoration: "none" }}>
+                <h4 className="nav-element">Home</h4>
+  
                 </Link>
-              </Typography>
 
-
-      
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                 <Link
                   to="/Allrecipes"
-                  style={{ color: "white", textDecoration: "none" }}
+                  style={{ color: "black", textDecoration: "none" }}
                 >
-                  All Recipes
+                <h4 className="nav-element">All Recipes</h4>
                 </Link>
-              </Typography>
+
+      </nav>
 
 
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </>
+      </div>
+
+    </header>
   );
 }
 
