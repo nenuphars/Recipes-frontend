5666e86555ea0cf1e46f8e5eb8dc44ee59bd88c3
Jorge Centeno Mesa <eljorgecenteno@gmail.com>
2024-02-27 21:47:30 +0100

    new bar finished

--- src/Pages/AllRecipesPage.jsx
+++ src/Pages/AllRecipesPage.jsx
@@ -2,23 +2,18 @@ import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
 import "../App.css";
-import Card from '@mui/material/Card';
+import Card from "@mui/material/Card";
 import { Link } from "react-router-dom";
-
+import SearchBar from "../Components/SearchBar";
 
 function AllRecipesPage() {
   const [allRecipes, setAllRecipes] = useState([]);
-  const [filteredRecipes, setFilteredRecipes] = useState([]);
-
-
-console.log(filteredRecipes)
+ 
   useEffect(() => {
-        axios
+    axios
       .get(`${import.meta.env.VITE_BASE_URL}/Recipes`)
       .then((recipes) => {
         setAllRecipes(recipes.data);
-        setFilteredRecipes(recipes.data)
-        console.log(recipes.data);
       })
       .catch((error) => {
         console.log(error);
@@ -26,50 +21,33 @@ console.log(filteredRecipes)
   }, []);
 
 
-  function searchByName(nameImput) {
-    if (nameImput === ""){
-      setFilteredRecipes(allRecipes)
-    }
-    let resultFiltered = allRecipes.filter(recipe =>{
-     return recipe.name.toLowerCase().includes(nameImput)
-    
-    }) ;
-   
-    setFilteredRecipes(resultFiltered)
-    
-   }
-   
   return (
     <>
-
-<div>
-      <form >
-        <input id="search-bar" placeholder="Search recipe"type="text" name="search" onChange={(e)=>{searchByName(e.target.value)}}/>
-        <span id="clear-search" className="clear-search-icon" onclick="">✕</span>
-      </form>
-    </div>
-
+    <SearchBar></SearchBar>
+     
       <div id="eachRecipeContainer">
-        {filteredRecipes.map((eachRecipe) => {
-    
+        {allRecipes.map((eachRecipe) => {
           return (
-            <Link to={`/Allrecipes/${eachRecipe.id}`}  key={eachRecipe.id} style={{textDecoration:"none"}}>
-            <Card id="eachCard" >
-            
-              <img
-                id="eachPhoto"
-                src={eachRecipe.photo_URL}
-                alt={`${eachRecipe.name} dish`}
-              />
-              <h2>{eachRecipe.name}</h2>
+            <Link
+              to={`/Allrecipes/${eachRecipe.id}`}
+              key={eachRecipe.id}
+              style={{ textDecoration: "none" }}
+            >
+              <Card id="eachCard">
+                <img
+                  id="eachPhoto"
+                  src={eachRecipe.photo_URL}
+                  alt={`${eachRecipe.name} dish`}
+                />
+                <h2>{eachRecipe.name}</h2>
 
-              <h4 >⏱️ {eachRecipe.duration}</h4>
-              <div id="tagContainer">
-                {eachRecipe.tags.map((eachTag) => {
-                  return <div key={eachTag}>{eachTag}</div>;
-                })}
-              </div>
-            </Card>
+                <h4>⏱️ {eachRecipe.duration}</h4>
+                <div id="tagContainer">
+                  {eachRecipe.tags.map((eachTag) => {
+                    return <div key={eachTag}>{eachTag}</div>;
+                  })}
+                </div>
+              </Card>
             </Link>
           );
         })}
