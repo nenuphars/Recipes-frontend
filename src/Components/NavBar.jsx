52e38677845503d4a74ac73fd192ad97510e09e4
Jorge Centeno Mesa <eljorgecenteno@gmail.com>
2024-02-29 22:00:28 +0100

    new colors homepage and navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,29 +1,37 @@
 import { Link } from "react-router-dom";
-import './NavBar.css'
-
+import "./NavBar.css";
+import Logo from "../Photos/wtf.png";
 
 function Navbar() {
   return (
     <header>
-      <div id="navbar-container">
-      <nav id="nav-links-wrapper">
-                <Link to="/" style={{ color: "mediumspringgreen", textDecoration: "none" }}>
-                <h4 className="nav-element">Home</h4>
-  
-                </Link>
+      
+        <nav id="nav-links-wrapper">
+          <Link
+            to="/"
+            style={{ color: "mediumspringgreen", textDecoration: "none" }}
+          >
+            <img
+            id="logo"
+              src={Logo}
+              alt="What the fridge logo"
+              className="nav-element"
+            />
+          </Link>
+          <Link
+            to="/"
+            style={{ color: "black", textDecoration: "none" }}
+          >
+            <h4 className="nav-element">Home</h4>
+          </Link>
 
-                <Link
-                  to="/Allrecipes"
-                  style={{ color: "black", textDecoration: "none" }}
-                >
-                <h4 className="nav-element">All Recipes</h4>
-                </Link>
-
-      </nav>
-
-
-      </div>
-
+          <Link
+            to="/Allrecipes"
+            style={{ color: "black", textDecoration: "none" }}
+          >
+            <h4 className="nav-element">All Recipes</h4>
+          </Link>
+        </nav>
     </header>
   );
 }
