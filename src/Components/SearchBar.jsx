f7e40e72f9f92dbc5493485a84a7359cc1f888e1
Nadine Rangosch <nrangosch@gmail.com>
2024-03-02 17:50:14 +0100

    worked on styling for search bar

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -34,18 +34,25 @@ useEffect(() => {
   function nameSearch(e){
     e.preventDefault()
     setSearchType('name')
+    document.getElementById('search-by-name-wrapper').style.backgroundColor = '#dd596b'
+    document.getElementById('search-by-tag-wrapper').style.backgroundColor = '#5971dd'
 
   }
 
   function ingredientsSearch(e){
     e.preventDefault()
     setSearchType('ingredients')
-
+    document.getElementById('search-by-ingredient-wrapper').style.backgroundColor = '#dd596b'
+    document.getElementById('search-by-tag-wrapper').style.backgroundColor = '#5971dd'
+    document.getElementById('search-by-name-wrapper').style.backgroundColor = '#5971dd'
   }
 
   function tagsSearch(e){
     e.preventDefault()
     setSearchType('tags')
+    document.getElementById('search-by-tag-wrapper').style.backgroundColor = '#dd596b'
+    document.getElementById('search-by-name-wrapper').style.backgroundColor = '#5971dd'
+    document.getElementById('search-by-ingredient-wrapper').style.backgroundColor = '#5971dd'
   }
 
   // useEffect that keeps track of the search queries when they are typed
@@ -118,9 +125,9 @@ function filterSearchbar(){
     <div id="search-bar-container">
         <form id="search-form">
         <div id='search-type-wrapper'>
-          <div className='search-by-name-wrapper'><button className='search-selection-box' onClick={e => nameSearch(e)}>Search by Name</button></div>
-          <div className='search-by-ingredient-wrapper'><button className='search-selection-box' onClick={e => ingredientsSearch(e)}>Search by Ingredient</button></div>
-          <div className='search-by-tag-wrapper'><button className='search-selection-box' onClick={e => tagsSearch(e)}>Search by Tag</button></div>
+          <div className='search-type-wrapper'><button id='search-by-name-wrapper' className='search-selection-box' onClick={e => nameSearch(e)}>Search by Name</button></div>
+          <div className='search-type-wrapper'><button id='search-by-ingredient-wrapper' className='search-selection-box' onClick={e => ingredientsSearch(e)}>Search by Ingredient</button></div>
+          <div className='search-type-wrapper'><button id='search-by-tag-wrapper' className='search-selection-box' onClick={e => tagsSearch(e)}>Search by Tag</button></div>
         </div>
           <div id="search-bar" >
         
@@ -135,11 +142,11 @@ function filterSearchbar(){
                 setActiveQuery(e.target.value);
               }}
             />
-           {filteredRecipes.length <30  &&<CloseIcon onClick={filterSearchbar}></CloseIcon>}
+           {filteredRecipes.length < allRecipes.length  &&<CloseIcon onClick={filterSearchbar}></CloseIcon>}
 
           </div>
         </form>
-        {filteredRecipes.length < 30 && (
+        {filteredRecipes.length < allRecipes.length && (
       <div className="eachObjectContainer">
         {filteredRecipes.map((eachRecipe) => {
           return <Link to={`/Allrecipes/${eachRecipe.id}`} key={eachRecipe.id} style={{ color: "black", textDecoration: "none" }} ><p  id="eachObject">
