09b6a256543d877dad0f73349532046bb4043959
Jorge Centeno Mesa <eljorgecenteno@gmail.com>
2024-03-04 17:33:17 +0100

    finised FAQ page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import RecipeDetailsPage from './Pages/RecipeDetailsPage'
 import CreateRecipePage from './Pages/CreateRecipePage'
 import EditRecipe from './Pages/EditRecipe'
 import Dashboard from './Pages/Dashboard'
+import FAQPage from './Pages/FAQPage'
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
         <Route path='/Allrecipes' element={<AllRecipesPage />}></Route>
+        <Route path='/FAQ' element={<FAQPage />}></Route>
         <Route path='/Allrecipes/:id' element={<RecipeDetailsPage />}></Route>
         <Route path='/dashboard' element={<Dashboard />}></Route>
         <Route path='/dashboard/edit/:id' element={<EditRecipe />}></Route>
