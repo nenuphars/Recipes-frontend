f4b0ad7b8eb11ab1cc4229e3ffe6781d73b04c67
Nadine Rangosch <nrangosch@gmail.com>
2024-10-25 15:02:42 +0200

    updated styling on recipe details page

--- src/Pages/RecipeDetailsPage.jsx
+++ src/Pages/RecipeDetailsPage.jsx
@@ -1,73 +1,109 @@
-import { useParams } from "react-router-dom";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
-import "./RecipeDetailsPage.css";
+import './RecipeDetailsPage.css';
 // import ErrorPage from "./ErrorPage";
-import recipesService from "../services/recipes.services";
+import recipesService from '../services/recipes.services';
+import { Box, Container, Stack, Typography } from '@mui/material';
 
 function RecipeDetailsPage() {
   const { id } = useParams();
 
   const [recipe, setRecipe] = useState(null);
-  // const [fakeRecipe, setFakeRecipe] =useState(false)
 
   useEffect(() => {
-    recipesService.getRecipe(id)
+    recipesService
+      .getRecipe(id)
       .then((response) => {
         setRecipe(response.data);
       })
       .catch((error) => {
         console.log(error);
-        // setFakeRecipe(true)
       });
   }, [id]);
-  
 
   return (
-    <div id="backgroundDetails">
-      {/* {!recipe && fakeRecipe && <ErrorPage></ErrorPage>} */}
-      {!recipe  && <CircularProgress id="spiner-detailsPage" size={100} color="success"></CircularProgress>}
+    <div id="RecipeDetailsPage" className="page-wrapper">
+      {!recipe && (
+        <CircularProgress
+          id="spiner-detailsPage"
+          size={100}
+          color="success"
+        ></CircularProgress>
+      )}
       {recipe && (
         <div>
-          <div id="recipeContainer">
-            <div id="first-recipe-bloq">
-              <img
-                id="PhotoDetails"
-                src={recipe.photo_url}
-                alt="Photo of this dish"
-              />
-              <div id="first-recipe-bloq-text">
-                <h1>{recipe.name}<br></br><br></br></h1>
-                <h3>
-                  ⏱️{recipe.duration} mins
-                  <br></br>
-                  <br></br>
-                  <br></br>
+          <Container maxWidth="lg">
+            <Stack spacing={2} sx={{ l: { flexDirection: 'row' } }}>
+              <Stack
+                spacing={1}
+                sx={{ alignItems: 'center', maxWidth: '70vw' }}
+              >
+                <Typography variant="h2">{recipe.name}</Typography>
+                <Stack
+                  direction="row"
+                  spacing={2}
+                  sx={{ alignItems: 'center', justifyContent: 'center' }}
+                >
                   {recipe.tags.map((eachTag) => {
-                    return `${eachTag}    `;
+                    return (
+                      <Typography key={eachTag} variant="h6">
+                        {eachTag}
+                      </Typography>
+                    );
                   })}
-                </h3>
-                <h2>
-                  <br></br>
-                  <br></br>
-                  {recipe.description}
-                </h2>
-              </div>
-            </div>
-            <h2 className="subtitles2">Ingredients and preparation for {recipe.servings} servings:</h2>
-            <ul id="ingredientListDetails">
-              {recipe.ingredientsList.map((eachObject) => (
-                <li key={recipe.id}>
-                  {eachObject.ingredient_name} : {eachObject.ingredient_amount} {eachObject.ingredient_measuring}
-                </li>
-              ))}
-            </ul>
+                </Stack>
+                <Typography variant="h5">⏱️{recipe.duration} mins</Typography>
 
-            <div className="subtitles2">
-              Preparation: <h4 id="preparation">{recipe.preparation}</h4>
-            </div>
-          </div>
+                <Typography
+                  variant="h4"
+                  sx={{ textAlign: 'center', width: '80%' }}
+                >
+                  {recipe.description}
+                </Typography>
+              </Stack>
+              <Box
+                sx={{
+                  width: '70vw',
+                  height: '70vw',
+                  display: 'flex',
+                  overflow: 'hidden',
+                  borderRadius: '20px',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <img
+                  id="recipe-details-photo"
+                  src={recipe.photo_url}
+                  alt={`Photo of ${recipe.name}`}
+                />
+              </Box>
+
+              <Stack>
+                <Typography variant="h4">
+                  Ingredients for {recipe.servings} servings:
+                </Typography>
+                <ul id="recipe-details-ingredient-list">
+                  {recipe.ingredientsList.map((eachObject) => (
+                    <li key={recipe.id}>
+                      <Typography variant="h6">
+                        {eachObject.ingredient_name} :{' '}
+                        {eachObject.ingredient_amount}{' '}
+                        {eachObject.ingredient_measuring}
+                      </Typography>
+                    </li>
+                  ))}
+                </ul>
+              </Stack>
+
+              <div className="recipe-details-preparation-container">
+                <Typography variant="h4">Preparation:</Typography>
+                <Typography variant="h6">{recipe.preparation}</Typography>
+              </div>
+            </Stack>
+          </Container>
         </div>
       )}
     </div>
