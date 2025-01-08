cca2fdad93c870d738d88c5aaaefebf16188511a
Nadine Rangosch <nrangosch@gmail.com>
2025-01-08 15:46:33 +0100

    cleaned up dependencies and mui versions

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', '@mui/material', '@emotion/react', '@emotion/styled'],
+        },
+      },
+    }
+  },
   resolve: {
     alias: {
       '@': '/src'
     }
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-  },
-  server: {
-    port: 3000
   }
 })
